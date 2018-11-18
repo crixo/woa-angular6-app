fda118f7dd613a46d2b798bb9479532f5b42e1b8
cristiano-degiorgis <cri@webprofessor.it>
2018-11-18 19:58:06 +0100

    heroku deploy

--- heroku-container-release.sh
+++ heroku-container-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker push registry.heroku.com/$REPO/web;
-imageId=$(docker inspect registry.heroku.com/$APP_NAME/web --format={{.Id}})
+imageId=$(docker inspect registry.heroku.com/$REPO/web --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
-curl -n -X PATCH https://api.heroku.com/apps/microservice-60min/formation \
+curl -n -X PATCH https://api.heroku.com/apps/$REPO/formation \
 -d "$payload" \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
