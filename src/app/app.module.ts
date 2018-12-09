3f3b1385e286d4bf884e8fb12bc1b21a36f59419
cristiano-degiorgis <cri@webprofessor.it>
2018-12-09 13:54:11 +0100

    [CI SKIP]clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 import { EnvServiceProvider } from './services/env.service.provider';
 import { StorageServiceModule } from 'angular-webstorage-service';
 import { LocalStorageService } from './services/local-storage.service';
 import { HttpErrorHandler } from './services/http-error-handler.service';
-
 //import './string-prototypes';
-
 
 @NgModule({
   imports:[ 
